84b428c5eb56582dbbf162bebbf8c87914101e10
nickolasbradham <nickolasbradham@gmail.com>
2022-07-29 10:46:51 -0600

    Fixed filename.

--- src/downloader/Downloader.java
+++ src/downloader/Downloader.java
@@ -14,14 +14,15 @@ final class Downloader {
 
 	private Downloader() throws MalformedURLException, IOException {
 		raw = new String(
-				new URL("https://mods.factorio.com/api/mods?version=1.1&page_size=max").openStream().readAllBytes());
+				new URL("https://mods.factorio.com/api/mods?version=1.1&page_size=2").openStream().readAllBytes());
 	}
 
-	private synchronized String next() {
+	private synchronized String[] next() {
 		parsed = raw.indexOf("\"download_url\": \"", parsed) + 17;
 
 		if (parsed >= 17)
-			return raw.substring(parsed, parsed = raw.indexOf('"', parsed + 1));
+			return new String[] { raw.substring(parsed, parsed = raw.indexOf('"', parsed + 1)), raw.substring(
+					parsed = raw.indexOf("\"file_name\": \"", parsed) + 14, parsed = raw.indexOf('"', parsed + 1)) };
 
 		parsed = raw.length();
 		return null;
@@ -35,15 +36,14 @@ final class Downloader {
 		Thread[] threads = new Thread[Runtime.getRuntime().availableProcessors()];
 		for (byte i = 0; i < threads.length; i++) {
 			threads[i] = new Thread(() -> {
-				String s;
+				String[] s;
 				while ((s = next()) != null)
 					try {
-						String name = s.substring(s.indexOf('/', 1) + 1, s.lastIndexOf('/'));
-						FileOutputStream fos = new FileOutputStream(new File(saveLoc, name + ".zip"));
-						System.out.println("Downloading: " + name);
+						FileOutputStream fos = new FileOutputStream(new File(saveLoc, s[1]));
+						System.out.println("Downloading: " + s[1]);
 						fos.getChannel()
 								.transferFrom(
-										Channels.newChannel(new URL("https://mods.factorio.com" + s + "?username="
+										Channels.newChannel(new URL("https://mods.factorio.com" + s[0] + "?username="
 												+ Login.USERNAME + "&token=" + Login.TOKEN).openStream()),
 										0, Long.MAX_VALUE);
 						fos.close();
